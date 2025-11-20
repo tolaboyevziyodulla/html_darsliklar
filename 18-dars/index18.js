cc802a8aa9ef83f747587136587b30f608d30413
Ziyodulla Hecker <tolaboyevziyodulla@gmail.com>
2025-11-20 19:26:21 +0500

    18-dars yuklandi
    Bu darsda biz JSga o'tamiz

---
+++ 18-dars/index18.js
@@ -0,0 +1,29 @@
+a = 'Tulabayev'
+b = 'Ziyodulla'
+c = 20
+f = 11
+console.log(a + ' ' + b + c + f)
+
+
+
+y = '60'
+
+if (60 === y) {
+	console.log('katta')
+} else {
+	console.log('tipmas' )
+}
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(f))
+console.log(typeof(y))
+
+ h = 90
+ o = 80
+ max = (o + h) / 2 + Math.abs((o - h) / 2)
+ min = (o + h) / 2 - Math.abs((o - h) / 2)
+
+ console.log('Maximum: ', max)
+ console.log('Minimum: ', min)
