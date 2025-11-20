17122f800bcb73cb3c7ce53380dc1f394de65f3b
Ziyodulla Hecker <tolaboyevziyodulla@gmail.com>
2025-11-20 19:27:02 +0500

    19-dars yuklandi

---
+++ 19-dars/index19.js
@@ -0,0 +1,19 @@
+day = 9;
+
+switch(day) {
+	case 1: console.log('Dushanba'); break;
+	case 2: console.log('Seshanba'); break;
+	case 3: console.log('Chorshanba'); break;
+	case 4: console.log('Pashanba'); break;
+	case 5: console.log('Juma'); break;
+	case 6: console.log('Shanba'); break;
+	case 7: console.log('Yakshanba'); break;
+	default: console.log('Bunday kun mavjud emas!'); break;
+}
+a = 10
+
+for (i = 0; i < 47; i ++) {
+	a ++;
+	a %= 2
+}
+console.log(a)
